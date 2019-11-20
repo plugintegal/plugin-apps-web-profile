a61386c0c72ea6af334bf6c622ca69aecd35ab6c
faqihakih <faqihakih12@gmail.com>
2019-11-20 20:30:52 +0700

    edit pakege json

--- public/js/app.js
+++ public/js/app.js
@@ -51218,8 +51218,8 @@ var router = new vue_router__WEBPACK_IMPORTED_MODULE_1__["default"]({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/felix/Documents/plugin-apps-web-profile/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/felix/Documents/plugin-apps-web-profile/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/faqih/Documents/webprofile-plugin/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/faqih/Documents/webprofile-plugin/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
